962ea886d769cbf33b177be1755d1252ca9906f6
Karl Entwistle <karl@entwistle.com>
2017-02-02 19:33:33 +0000

    Add routing constraints to RSVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :rsvps, path: 'rsvp'
+  resources :rsvps,
+    path: 'rsvp',
+    constraints: { id: /enter_code|attendance|food|confirmation|wicked_finish/ }
 end
