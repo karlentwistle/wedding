e68b8b2362f2fe52039297390d5cec6c1df7e236
Karl Entwistle <karl@entwistle.com>
2017-02-19 21:23:18 +0000

    Style error pages
    This obviously isn’t the best way to do error pages in Rails because…..
    
    DRAWBACKS
    
    Dynamic error pages let us use the power of the Rails view layer, but this has
    its own drawbacks.
    
    If the error page has errors. Syntax errors, database outages, or other
    catastrophes can lead to dynamic error pages that themselves fail to render.
    If this happens, not only can’t users interact with your app, they won’t be
    able to see your fancy error page!
    
    Luckily Rails is smart enough to recognise this situation an avoid an infinite
    loop. As a last resort, Rails will display a simple plaintext error message:
    
    500 Internal Server Error
    
    If you are the administrator of this website, then please read this web
    application’s log file and/or the web server’s log file to find out what went
    wrong.
    
    https://mattbrictson.com/dynamic-rails-error-pages
    
    Since this is my personal wedding website I’m going to take a calculated risk
    and just use dynamic error with no fallback.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :rsvps,
     path: 'rsvp',
     constraints: { id: /enter_code|attendance|food|confirmation|wicked_finish/ }
+
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 end
