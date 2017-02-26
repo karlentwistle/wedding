071461c46d517def3d5c94da6075384a0210ba00
Karl Entwistle <karl@entwistle.com>
2017-02-26 15:54:07 +0000

    Moar Styling tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     path: 'rsvp',
     constraints: { id: /enter_code|attendance|food|confirmation|wicked_finish/ }
 
-  match "/404", :to => "errors#not_found", :via => :all
-  match "/500", :to => "errors#internal_server_error", :via => :all
+  if !Rails.env.development?
+    match "/404", :to => "errors#not_found", :via => :all
+    match "/500", :to => "errors#internal_server_error", :via => :all
+  end
 end
