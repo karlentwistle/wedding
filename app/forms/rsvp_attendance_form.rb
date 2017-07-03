14a07dc0d76cb2af73a3b21e4384fe18eb7b5c23
Karl Entwistle <karl@entwistle.com>
2017-07-03 22:23:07 +0100

    Refactor person validation of attendance form

--- app/forms/rsvp_attendance_form.rb
+++ app/forms/rsvp_attendance_form.rb
@@ -2,6 +2,8 @@ class RsvpAttendanceForm < RsvpBaseForm
   include ActiveModel::Validations
   include ActiveModel::Conversion
 
+  validate :validate_people
+
   delegate :people, to: :rsvp_code, prefix: false, allow_nil: false
   delegate :ceremony, :reception, to: :rsvp_code, prefix: false, allow_nil: false
 
@@ -19,14 +21,6 @@ class RsvpAttendanceForm < RsvpBaseForm
     end
   end
 
-  def valid?
-    super && all_people_valid?
-  end
-
-  def all_people_valid?
-    people.map(&:valid?).all? {|p| p == true}
-  end
-
   def people_attributes=(people_attributes)
     people_attributes.to_h.each do |_, person_attributes|
       person = people.find(
@@ -37,6 +31,14 @@ class RsvpAttendanceForm < RsvpBaseForm
 
       person.attending_reception = person_attributes[:attending_reception]
       person.attending_ceremony = person_attributes[:attending_ceremony]
+    end
+  end
+
+  private
+
+  def validate_people
+    unless people.map(&:valid?).all?
+      error.add(:base, 'invalid person')
     end
   end
 end
